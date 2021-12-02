0b58353e883161914ce4b4028acc5da14ae39978
Yunus Ergül <56773451+yunusergul@users.noreply.github.com>
2021-12-02 12:10:36 +0300

    Create Taks1.sql

---
+++ SQL/Taks1.sql
@@ -0,0 +1,16 @@
+/* Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
+film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
+film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
+customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
+film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
+Kolay Gelsin. */
+
+select title, description  from film;
+select *  from film where length<75 and length>60 ;
+select * from film where rental_rate=0.99 and (replacement_cost =12.99 or replacement_cost=28.99);
+select last_name from customer where first_name='Mary';
+select title, description from film where length<50 and  rental_rate != 2.99 and rental_rate != 4.99 ;
+
+
